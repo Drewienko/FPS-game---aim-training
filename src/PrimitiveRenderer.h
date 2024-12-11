db5945a6621d7be7f9b1f820a0aab5442cc082be
Piotr Thomas <pioterk906@gmail.com>
2024-12-12 00:19:39 +0100

    komentarze

--- src/PrimitiveRenderer.h
+++ src/PrimitiveRenderer.h
@@ -3,16 +3,13 @@
 
 class PrimitiveRenderer {
 public:
-    // Draw points using vertex and color arrays
+
     static void drawPoints(float* vertices, float* colors, int count);
 
-    // Draw lines using vertex and color arrays
     static void drawLines(float* vertices, float* colors, int count);
 
-    // Draw triangles using vertex and color arrays
     static void drawTriangles(float* vertices, float* colors, int count);
 
-    // Draw a cube with size, position, and color
     static void drawCube(float size, float x, float y, float z, float* color);
 };
 
